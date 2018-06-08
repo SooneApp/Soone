29c246ddedcc6570e94e99ea1369246cca5b3355
Unknown <poirot.nans@gmail.com>
2018-06-08 14:50:10 +0200

    Add : models

---
+++ soone-backend/api/models/SexInterest.js
@@ -0,0 +1,23 @@
+/**
+ * SexInterest.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+        id: {
+            type: "number",
+            autoIncrement: true,
+            primaryKey: true
+        },
+        label: {
+            type: "string",
+            columnType: "varchar(16)"
+        },
+    },
+
+};
+
